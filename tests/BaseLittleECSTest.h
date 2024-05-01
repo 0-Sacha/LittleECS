05b1cd33a86d5296d48befc654ed81ebab5c98f6
Sacha Bellier <bellier.sacha2@gmail.com>
2024-05-01 16:22:58 +0200

    fix: delete tests

--- tests/BaseLittleECSTest.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "ProjectCore/Tester/TestSuite/AllTestSuite.h"
-
-inline PCT_TEST_SUITE(LITTLE_ECS);
--- tests/Tests.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "ProjectCore/Tester/TestSuite/AllTestSuite.h"
-
-int main()
-{
-    ProjectCore::Tester::TestSuitesManager::Verbose = false;
-    return ProjectCore::Tester::TestSuitesManager::ExecAllTestSuites();
-}
